a262392ca1c4019495b518137725cb8d7ccdbc7d
wpplumber <mail.tarik.rital@gmail.com>
2024-10-21 15:04:10 +0100

    feat: Try 1 to fix bug with assets path

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://wpplumber.github.io',
+	site: 'https://wpplumber.github.io/astroboard',
     base: 'astroboard',
 	outDir: 'docs',
       image: {
